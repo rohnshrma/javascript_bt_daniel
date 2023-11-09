b9808ae99e0148b6ee20bc0d21b6e39d1e64cfbd
rohnshrma <rohn11shrma@gmail.com>
2023-11-09 23:42:15 +0530

    variables and data types(primitive)

---
+++ script.js
@@ -0,0 +1,127 @@
+// how to provide output in js ?
+
+// 1. console.log()
+
+// console.log(12+23)
+
+
+// 2. alert / window.alert() 
+
+// window.alert(12+34)
+
+
+// 3. document.write()
+
+// document.write("<h1>bye bye world</h1>")
+
+
+
+// how to take input from a user
+
+// prompt() function is used to take input from the user
+
+// prompt("Enter your name", "john doe") // message (optional) , default (optional)
+
+
+// how to store values in js
+
+// variables are empty containers that allows us to store data in them.
+
+// how to create a variable ?
+
+// var keyword is used to create a variable in js , followed by unique name (Variable name / identifier)
+
+
+
+// app 1
+
+// declaration (creation)
+// var myname;
+
+// intialization (assigning a value)
+// myname = "daniel";
+
+
+
+// app 2
+// var yourName = "rohan sharma";
+
+
+// benifit 
+// reusability , organize the code
+
+// console.log("hello world my name is " + myname);
+
+
+// var mess = prompt("Enter your message : ")
+// console.log("the message provided by daniel => " + mess);
+
+
+
+// variable naming conventions
+
+// no space
+// var my name ;
+
+
+
+// no use of reserved keywords
+// var  var;
+// var for;
+
+// a variable name cannot start with a number but can include or end with a number
+
+// var 1name;
+// var name1;
+// var name1my;
+
+
+// a variable name cannot start , include or end with a apecial symbol except "_" and "$"
+
+
+// var @yourName;
+
+// var $myage;
+
+// var _my_name_is;
+
+
+
+
+// in case of multiword variable name use
+
+// var hellomynameis;
+
+// var hello_my_name_is; //snake_casing
+
+// var helloMyNsmeIs; // camelCasing
+
+
+
+
+// data types
+
+
+// Primitive (simple)
+
+// String
+// Numbers : 1. Integers 2. float 
+// Boolean : true or false
+// null
+// undefined
+
+// symbol
+ 
+
+// variable is there but no value has been assigned to it yet
+// unintentional absense of a value
+
+// var x;
+// console.log(x);
+
+
+// null => intentional absense of a value
+
+// var future = null;
+
+// var y = null;
