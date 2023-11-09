f240130939db9101a875319b9ff1f7f9646bf9ea
rohnshrma <rohn11shrma@gmail.com>
2023-11-10 00:03:02 +0530

    string intro (upto template literals)

---
+++ strings.js
@@ -0,0 +1,56 @@
+// how to check the data type of any value ?
+// "typeof" keyword returns the data type of the value / variable 
+
+
+
+// var mess = "abc is a name";
+
+// console.log(typeof mess);
+
+
+// console.log(typeof 123);
+
+
+// console.log(typeof true);
+
+
+// a string is a sequence of characters (alphabets, numbers and special symbols 
+// (including whitespace)) enclosed in single ('') or double ("") quotes
+
+
+
+// console.log(typeof "hello world");
+// console.log(typeof "1234");
+// console.log(typeof "rohan@124");
+// console.log(typeof 123);
+// console.log(typeof "true");
+
+
+// no matter what typeof value you take as input using prompt , the data type of it will be string by default
+
+// var age = prompt("Enter your age : ")
+// console.log(typeof age)
+
+// var age = parseInt(prompt("Enter your age : "))
+// console.log(typeof age)
+
+
+
+
+// var first_name = prompt("Enter your first name ")
+// var last_name = prompt("Enter your last name ")
+// var age = prompt("Enter your age ")
+
+// concatenation
+// var mess = "hello world my name is " + first_name + " " + last_name + " and my age is " + age
+
+
+
+// template literal => backticks (``) to create a template literal
+// ${} to insert js expression / variables
+
+// insertion / injection
+
+// var mess = `hello world my name is ${first_name} ${last_name} and my age is ${age*2}`
+
+// console.log(mess);
